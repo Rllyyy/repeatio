4aee9b026c878e34c9d0fd41afb0d1ea4f64694c
Niklas Fischer <nikii17.06@gmail.com>
2023-12-26 21:18:39 +0100

    :sparkles:Add news page (#359)

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,13 +1,25 @@
-import { SiteHeading } from "../components/SiteHeading/SiteHeading";
+import { Suspense, lazy } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { DefaultFallbackComponent } from "../components/SuspenseWithErrorBoundary";
+import { LoadingNewsSkeleton } from "../components/News/Loading";
+
+// CSS
+import styles from "../components/News/news.module.css";
+
+const NewsComponent = lazy(() => import("../components/News/NewsItems"));
 
 export default function NewsPage() {
   return (
     <>
-      <SiteHeading title='News' />
-      <p style={{ marginTop: "20px" }}>There seems to be nothing here...</p>
-      <p>
-        Visit <a href='https://github.com/Rllyyy/repeatio'>Github</a> to view the current status of this project.
-      </p>
+      <ErrorBoundary FallbackComponent={DefaultFallbackComponent}>
+        <div className={styles.headingWrapper}>
+          <h1>News</h1>
+          <p className={styles.headingWrapper__subheading}>The latests updates and improvements to repeatio</p>
+        </div>
+        <Suspense fallback={<LoadingNewsSkeleton />}>
+          <NewsComponent />
+        </Suspense>
+      </ErrorBoundary>
     </>
   );
 }
