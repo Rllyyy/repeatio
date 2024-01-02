0285f0d1a84d5a3675c13c45c2cf53c1bd2d5d24
Niklas Fischer <nikii17.06@gmail.com>
2024-01-02 20:12:15 +0100

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1792px",
+      "4xl": "2048px",
+    },
+  },
+  plugins: [],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+};
