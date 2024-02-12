c89a7894eb399158c3c4dd7a86bd915cbc4d2f68
Niklas Fischer <nikii17.06@gmail.com>
2024-02-12 16:26:12 +0100

    Remove __dirname from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
 import eslint from "vite-plugin-eslint";
 import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
