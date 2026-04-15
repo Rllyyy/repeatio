77766a4e0efadc92ab8e87ca5e7b8d2ba42abfa3
Niklas Fischer <nikii17.06@gmail.com>
2026-04-15 21:23:08 +0200

    remove exclude

--- cypress.config.ts
+++ cypress.config.ts
@@ -24,9 +24,6 @@ export default defineConfig({
           host: true,
           port: 3000,
         },
-        optimizeDeps: {
-          exclude: ["react/jsx-dev-runtime"],
-        },
         build: {
           outDir: "build", // Changed output folder, like in CRA
         },
