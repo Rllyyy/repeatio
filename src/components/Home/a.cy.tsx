5c665eba6f1c216998457f75be498d38d6da15e4
Niklas Fischer <nikii17.06@gmail.com>
2026-04-15 20:25:05 +0200

    remove test

--- src/components/Home/a.cy.tsx
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-//  Warumup for vite
-
-describe("Vite warmup", () => {
-  it("always passes", () => {
-    // cy.mount(<Home />);
-    expect(true).to.equal(true);
-  });
-});
